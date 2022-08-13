77003ebc9fc599a7a280961b9399c240a4692b90
A-Tamura15 <108603835+A-Tamura15@users.noreply.github.com>
2022-08-13 21:48:50 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Practice1 } from "./placticies/Practice1";
+import { Practice2 } from "./placticies/Practice2";
+import { Practice3 } from "./placticies/Practice3";
+import { Practice4 } from "./placticies/Practice4";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Practice1 />
+      <Practice2 />
+      <Practice3 />
+      <Practice4 />
+    </div>
+  );
+}
